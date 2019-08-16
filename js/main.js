3b2a3b41af081e2b20f497e8e6f5dc03d4226012
vaibhav <joshi.vaibhav.j@gmail.com>
2019-08-16 18:08:32 +0800

    crated mor game logic

--- js/main.js
+++ js/main.js
@@ -1,7 +1,27 @@
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
+
+cards = ["queen", "queen", "king", "king"];
+cardsInPlay = [ ];
+
+cardOne = cards[0];
+cardsInPlay.push(cardOne);
+
+
+cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+let cardsInPlay;
+if (cardsInPlay[0] === cardsInPlay[3]) {
+alert "you found a match";
+}
+else {
+alert "Sorry try again";
+}
+
+
+
+
+
 
 console.log("User flipped " + cardOne);
-console.log("User flipped " + cardFour);
+console.log("User flipped " + cardTwo);
+alert('Hello, friends.');
